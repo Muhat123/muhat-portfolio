4c124a2e348a7e501ace5726a989af016d55e449
Muhammad_Abduttawwab <abduttawwabmuhammad@gmail.com>
2024-08-12 13:45:30 +0700

    first commit

---
+++ src/components/test.jsx
@@ -0,0 +1,7 @@
+const TestComponent = () => {
+    console.log("TestComponent is rendering");
+    return <div style={{ color: "red", fontSize: "20px", padding: "20px" }}>Test Component</div>;
+  };
+  
+  export default TestComponent;
+  
